24295824cd2fb2680c7b01df681f878f7d2ae7ab
yangmaolin <yangmaolin@troila.com>
2021-06-09 16:46:11 +0800

    feat: opt demo

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -13,6 +16,8 @@ const (
 var (
 	Second = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"}
 	Last   = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+	stopCh = make(chan struct{}, 1)
 )
 
 func RandInt64(min, max int64) int64 {
@@ -29,6 +34,29 @@ func GenLicensePlate() string {
 }
 
 func main() {
-	fmt.Println("Welcome to our park:", GenLicensePlate())
 
+	fmt.Println("Welcome to our park:", GenLicensePlate())
+	go InitSignal()
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("Welcome to our park:", GenLicensePlate())
+
+		case <-stopCh:
+			fmt.Println("Our park is out of service")
+			return
+		}
+	}
+}
+
+func InitSignal() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2)
+	for {
+		<-c
+		fmt.Println("Our park get a signal")
+		close(stopCh)
+		return
+	}
 }
